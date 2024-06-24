c9b1f404bac1c861dcd642ee08c44690e08cbcf0
alysonfernandesav <alysonfernandes@msn.com>
2024-06-23 21:50:16 -0300

    Create ex006 - Dobro Triplo e Raiz Quadrada.py

---
+++ ex006 - Dobro Triplo e Raiz Quadrada.py
@@ -0,0 +1,6 @@
+n1 = int(input('digie um numero'))
+d = n1*2
+t = n1*3
+r = n1**(1/2)
+#r = pow (n1, (1/2))
+print('O dobro do numero {} é {}, \n o seu triplo é {} \n e a sua raiz é {:.3}'.format(n1,d,t,r))
