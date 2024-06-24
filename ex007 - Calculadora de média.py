e471572ee0c16d0a7a3c262a7907830086683c23
alysonfernandesav <alysonfernandes@msn.com>
2024-06-24 18:37:26 -0300

    Create ex007 - Calculadora de média.py

---
+++ ex007 - Calculadora de média.py
@@ -0,0 +1,5 @@
+# Faça O Programa Que Leia As Duas Notas De Um Aluno, Calcule E Mostre A Sua Media 
+n1 = float(input('insira a primeira nota'))
+n2 = float(input('insira a segunda nota'))
+média = ((n1+n2)/2)
+print ('A nota média entre {:.1f} e {:.1f} é {:.1f}'.format(n1, n2, média))
