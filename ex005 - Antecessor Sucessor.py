1e6a8908b85b64ecc5537376b2fb33b328bc747a
alysonfernandesav <alysonfernandes@msn.com>
2024-06-23 21:39:45 -0300

    Create ex005 - Antecessor Sucessor.py

---
+++ ex005 - Antecessor Sucessor.py
@@ -0,0 +1,6 @@
+n1 = int(input('digite um numero'))
+#s: int = n1+1
+#a: int = n1-1
+#print('O antecessor do numero {} é {} e o sucessor é {}'.format(n1,a,s))
+print('\033[7;46mO antecessor do numero {} é {} e o sucessor é {}'.format(n1, (n1-1), (n1+1)))
+# posso escrever esse código dos dois jeitos, mas se precisar das variáveis depois precisarei guardar
